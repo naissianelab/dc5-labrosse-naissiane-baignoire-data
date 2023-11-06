a6ab49958fdbb4a4bc07b5628affb689e491dd0d
naissianelab <naisianelabrosse@gmail.com>
2023-11-06 11:29:41 +0100

    Nouvelle version qui permet d'afficher un message d'erreur si du texte est entré plutôt qu'un nombre

--- ex1-a.py
+++ ex1-a.py
@@ -1,7 +1,11 @@
-nom_entrepise = input("Rentrez le nom de l'entreprise : ")
-nombre_clients = int(input("Rentrez le nombre de clients : "))
-revenu_moyen = float(input("Rentrez le revenu moyen par client : "))
+nom_entreprise = input("Rentrez le nom de l'entreprise : ")
 
-revenu_total = revenu_moyen * nombre_clients
+try:
+    nombre_clients = int(input("Rentrez le nombre de clients : "))
+    revenu_moyen = float(input("Rentrez le revenu moyen par client : "))
+    
+    revenu_total = revenu_moyen * nombre_clients
+    print(f"Le revenu total de l'entreprise {nom_entreprise} est de : {revenu_total}")
 
-print(f"Le revenu total de l'entreprise est de : {revenu_total} ")
+except ValueError:
+    print("Erreur : Vous avez entré un texte au lieu d'un nombre pour le nombre de clients ou le revenu moyen.")
