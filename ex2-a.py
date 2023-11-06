e8bf8a3802cb515ca14e30c2af410c2f9ec442c5
naissianelab <naisianelabrosse@gmail.com>
2023-11-06 10:22:35 +0100

    Calcul des dépenses marketing totales de l'année

---
+++ ex2-a.py
@@ -0,0 +1,6 @@
+depenses_mensuelles = [800, 120, 1200, 87, 400, 368, 489, 355, 700, 900, 379, 886]
+
+depenses_totales = 0
+
+for depense_mensuelle in depenses_mensuelles:
+    depenses_totales += depense_mensuelle
