f020a1e00787a83a72db23dbeb314782fba85e33
naissianelab <naisianelabrosse@gmail.com>
2023-11-12 20:13:39 +0100

    liste des dépenses marketing

--- ex2-a.py
+++ ex2-a.py
@@ -1,6 +1,8 @@
-depenses_mensuelles = [800, 120, 1200, 87, 400, 368, 489, 355, 700, 900, 379, 886]
-
+depenses_mensuelles = [800, -120, 1200, 87, 0.5, 368, 489, 355, 700, 900, "ok", 886]
 depenses_totales = 0
 
 for depense_mensuelle in depenses_mensuelles:
-    depenses_totales += depense_mensuelle
+    if isinstance(depense_mensuelle, (int, float)):
+        depenses_totales += depense_mensuelle
+
+print(f"Les dépenses marketing de l'années sont de : {depenses_totales}")
