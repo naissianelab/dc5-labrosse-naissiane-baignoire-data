33fac51aa1a8c275c9351719da692cd07394b1d9
naissianelab <naisianelabrosse@gmail.com>
2023-11-12 20:45:09 +0100

    Fonction pour convertir le CTR en pourcentage

--- ex4-a.py
+++ ex4-a.py
@@ -1,4 +1,9 @@
 def convertir_ctr_en_pourcentage(ctr):
+
+    if not isinstance(ctr, (int, float)):
+        print("Erreur : Le CTR doit être un nombre.")
+        return None
+    
     pourcentage_ctr = ctr * 100
     return pourcentage_ctr
 
